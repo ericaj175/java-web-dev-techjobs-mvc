33a9ad74446951f16bf1c56ad33612c1bf7e7d51
Erica Jones <61997520+ericaj175@users.noreply.github.com>
2020-06-23 12:43:45 -0500

    Update SearchController.java

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -1,5 +1,4 @@
 package org.launchcode.javawebdevtechjobsmvc.controllers;
-
 import org.launchcode.javawebdevtechjobsmvc.models.Job;
 import org.launchcode.javawebdevtechjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
@@ -21,8 +20,22 @@ public class SearchController {
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
         return "search";
-    }
+}
 
+    @PostMapping(value ="results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
+       ArrayList<Job> jobs;
+        if (searchType.toLowerCase().equals("all") || searchType.equals ("")){
+          jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        return "search";
+    }
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
 }
+
